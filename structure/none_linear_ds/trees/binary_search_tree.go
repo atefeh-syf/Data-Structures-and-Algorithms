ff8db09c7847bb6f73722588409673ec56e9b532
atefeh-syf <atefe.s.77218@gmail.com>
2024-04-06 00:05:26 +0330

    none linear data structure : tree add element

---
+++ structure/none_linear_ds/trees/binary_search_tree.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"sync"
+)
+
+type TreeNode struct {
+	key       int
+	value     int
+	leftNode  *TreeNode
+	rightNode *TreeNode
+}
+
+type BinarySearchTree struct {
+	rootNode *TreeNode
+	lock     sync.RWMutex
+}
+
+func (tree *BinarySearchTree) InsertElement(key int, value int) {
+	tree.lock.Lock()
+	defer tree.lock.Unlock()
+
+	var treeNode = &TreeNode{key, value, nil, nil}
+	if tree.rootNode == nil {
+		tree.rootNode = treeNode
+	} else {
+		insertTreeNode(tree.rootNode, treeNode)
+	}
+}
+
+func insertTreeNode(rootNode *TreeNode, newTreeNode *TreeNode) {
+	if newTreeNode.key < rootNode.key {
+		if rootNode.leftNode == nil {
+			rootNode.leftNode = newTreeNode
+		} else {
+			insertTreeNode(rootNode.leftNode, newTreeNode)
+		}
+	} else {
+		if rootNode.rightNode == nil {
+			rootNode.rightNode = newTreeNode
+		} else {
+			insertTreeNode(rootNode.rightNode, newTreeNode)
+		}
+	}
+}
+
+func (tree *BinarySearchTree) InOrderTraverseTree(functions func(int)) {
+	tree.lock.Lock()
+	defer tree.lock.Unlock()
+	inOrderTraverseTree(tree.rootNode, functions)
+}
+
+func inOrderTraverseTree(treeNode *TreeNode, function func(int)) {
+	if treeNode != nil {
+		inOrderTraverseTree(treeNode.leftNode, function)
+		function(treeNode.value)
+		inOrderTraverseTree(treeNode.rightNode, function)
+	}
+}
+
+func main() {
+
+}
